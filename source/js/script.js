b91d134651296e38f22d5993a123c4f653411765
Tina Kuzmenko <tina.kuzmenko@gmail.com>
2020-01-13 00:14:00 +0100

    Fixed bug with scroll

--- source/js/script.js
+++ source/js/script.js
@@ -1,17 +1,17 @@
 var templateContent = document.querySelector("#message-template").content;
 var message = templateContent.querySelector(".chat-message");
 var chatContainer = document.querySelector(".chat-content");
-var chatInput = document.querySelector(".chat-form-input");
-var chatButtonSend = document.querySelector(".chat-form-button");
+var chatInput = document.querySelector(".chat-form__input");
+var chatButtonSend = document.querySelector(".chat-form__button");
 var chatMessages = chatContainer.children;
-var chatDeleteButton = document.querySelector(".chat-message-button");
+var chatDeleteButton = document.querySelector(".chat-message__button");
 
 var createNewMessage = function() {
   var chatInputContent = chatInput.value;
   var newMessage = message.cloneNode(true);
-  var newMessageContent = newMessage.querySelector(".chat-message-text");
+  var newMessageContent = newMessage.querySelector(".chat-message__text");
   newMessageContent.textContent = chatInputContent;
-  var deleteButton = newMessage.querySelector(".chat-message-button");
+  var deleteButton = newMessage.querySelector(".chat-message__button");
   deleteButton.addEventListener("click", function() {
     newMessage.remove();
   });
